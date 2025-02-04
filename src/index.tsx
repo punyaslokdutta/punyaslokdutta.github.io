d62513dad2b7ea004ca755fb08992b11cbbe7f67
swayamsiddhadutta <swayamdutta16121997@gmail.com>
2025-02-04 21:46:10 +0530

    Converted to PWA

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below.
+serviceWorkerRegistration.register(); 
